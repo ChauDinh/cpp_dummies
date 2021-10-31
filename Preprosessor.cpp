e8ce4d39694f7e3debac450d76648976ec9f7304
Chau <baochau.dinh@vexere.com>
2021-10-31 18:35:31 +0700

    c++/template

---
+++ Preprosessor.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#define TOREPLACE REPLACEWITH
+#define rep(i, a, b) for (int i = a; i < b; i++)
+
+using namespace std;
+
+int main() {
+  rep(i, 0, 5) {
+    cout << i << endl;
+  };
+  return 0;
+}
+
